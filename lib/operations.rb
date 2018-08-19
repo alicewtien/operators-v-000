9f24c221429f667d6416771e9bcb7a539989e0cd
alice T <alicewtien@gmail.com>
2018-08-19 13:21:53 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,8 @@
 def unsafe?(speed)
-	
+	if "#{speed}".bewteen?(40, 60)
+		return false
+	else
+		return true
 end
 
 
@@ -8,6 +11,3 @@ def not_safe?(speed)
 	speed = (40, 60)
 	speed.between?(40, 60) ? "false" : "true"
 end
-	
-
-
